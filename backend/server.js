267e9be577fac22edbd23991efbdc9efbc7d0459
Nader Naguib <140602230+Nader-Naguib05@users.noreply.github.com>
2025-11-25 00:38:19 +0200

    Parsing fix

--- backend/server.js
+++ backend/server.js
@@ -20,8 +20,9 @@ const server = http.createServer(app);
 
 // middleware
 app.use(cors());
-app.use(bodyParser.json({ limit: '5mb' }));
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json({ limit: "10mb" }));
+app.use(express.urlencoded({ extended: true }));
+
 
 // routes
 app.use('/webhook', webhookRoutes);
