e451ba99537b1f8bae2a4bdfcd38fe1ac511ede5
Nader Naguib <140602230+Nader-Naguib05@users.noreply.github.com>
2025-11-25 16:24:55 +0200

    Fix: sendText

--- frontend/src/api/messages.js
+++ frontend/src/api/messages.js
@@ -1,6 +1,6 @@
 import api from "./axios";
 
 export const sendMessage = async (conversationId, text) => {
-  const { data } = await api.post("/messages/send", { conversationId, text });
+  const { data } = await api.post("/messages/sendText", { conversationId, text });
   return data;
 };
